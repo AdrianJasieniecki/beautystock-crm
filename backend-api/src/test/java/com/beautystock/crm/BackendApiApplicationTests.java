4448d2984063e94a3f4e85d381793bddc8a88ef9
AdrianJasieniecki <adrian.jasieniecki3@gmail.com>
2026-07-30 18:37:26 +0200

    test: remove unused imports in BackendApiApplicationTests

--- backend-api/src/test/java/com/beautystock/crm/BackendApiApplicationTests.java
+++ backend-api/src/test/java/com/beautystock/crm/BackendApiApplicationTests.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.postgresql.PostgreSQLContainer;
@@ -20,7 +19,6 @@ import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.in;
 
 @Testcontainers
 @SpringBootTest
